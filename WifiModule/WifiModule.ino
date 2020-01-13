e8d017835d29f5d2164c9551c545795656622c3b
Zafer <zaferdal@yahoo.com>
2020-01-13 15:48:03 +0300

    persistent connection

--- WifiModule/WifiModule.ino
+++ WifiModule/WifiModule.ino
@@ -1,9 +1,11 @@
 #include <WiFi.h>
 
-#define WIFI_SSID     "Kablonet Netmaster-CA0B-A"
+#define WIFI_SSID     "Kablonet Netmaster-CA0B-G"
 #define WIFI_PASSWORD "ce1a5f35"
 
 #define RELAY_PIN 15
+
+WiFiClient client;
 
 void connectWiFi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -31,15 +33,18 @@ void setup() {
 void loop() {
   if (WiFi.status() != WL_CONNECTED) connectWiFi();
 
-   WiFiClient client;
-  if (!client.connect("192.168.0.12", 42024)) {
+  if (!client && !client.connect("192.168.0.12", 42024)) {
       Serial.println("connection failed");
       delay(2000);
       return;
   }
 
+  
   while(client.available()) {
+      Serial.println("connection");
       String line = client.readStringUntil('\n');
+
+      Serial.println(line);
 
       if (line.equals("ON")) digitalWrite(RELAY_PIN, LOW);
       else if (line.equals("OFF")) digitalWrite(RELAY_PIN, HIGH);
