5f5e76c0a9f51e4c4a99ca961a89160c57b053f1
paddlesteamer <umuter@protonmail.com>
2020-01-28 17:25:17 +0300

    log files separated

--- webcam.py
+++ webcam.py
@@ -4,11 +4,11 @@ import os
 
 
 def takePhoto():
-    os.system("fswebcam -r 640x480 --jpeg 85 -D 1 shot.jpg")
+    os.system("fswebcam -r 640x480 --jpeg 85 -D 1 shot.jpg > photo.log 2>&1")
 
     return 'shot.jpg'
 
 def shootVideo():
-    os.system("ffmpeg -t 10 -f v4l2 -framerate 25 -video_size 640x80 -i /dev/video0 video.mkv")
+    os.system("ffmpeg -t 10 -f v4l2 -framerate 25 -video_size 640x80 -i /dev/video0 video.mkv > video.log 2>&1")
 
     return 'video.mkv'
