d8c2a3fd04ccc10616dbfb0c3f71f01da6c2f282
paddlesteamer <umuter@protonmail.com>
2020-03-11 16:22:21 +0300

    video -> mp4

--- RaspberryPi/wificam.py
+++ RaspberryPi/wificam.py
@@ -124,7 +124,7 @@ class WifiCam(threading.Thread):
             frameId+=1
 
         frameRate = int(frameId / 10)
-        os.system("ffmpeg -t 10 -i frames/frame%04d.jpg -c:v libx264 -vf scale=640x48   0 frames/video.mp4")
+        os.system("ffmpeg -t 10 -i frames/frame%04d.jpg -c:v libx264 -vf scale=640x480 frames/video.mp4")
 
         f = open("frames/video.mp4", "rb")
         video = f.read()
