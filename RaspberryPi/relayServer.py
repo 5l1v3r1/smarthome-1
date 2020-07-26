a72e4d0791964ef6033b94bc74f278d15b014c1c
paddlesteamer <umuter@protonmail.com>
2020-07-26 13:08:26 +0300

    Connection close handle on wificam && polish

--- RaspberryPi/relayServer.py
+++ RaspberryPi/relayServer.py
@@ -11,41 +11,42 @@ class RelayServer(threading.Thread):
 
     def run(self):
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-            s.bind(("0.0.0.0", 42024))
-            s.listen()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        s.bind(("0.0.0.0", 42024))
+        s.listen()
+
+        while True:
+            conn, addr = s.accept()
+            print("[+] New connection to relay server from {0}.".format(addr))
+
+            conn.settimeout(0.1)
+
             while True:
-                conn, addr = s.accept()
-                print("[+] New connection to relay server from {0}.".format(addr))
+                if len(self._cmdQueue) == 0:
+                    try:
+                        data = conn.recv(256)
+                        while len(data) < 5:
+                            data += conn.recv(256)
 
-                conn.settimeout(0.1)
-                with conn:
-                    while True:
-                        if len(self._cmdQueue) == 0:
-                            try:
-                                data = conn.recv(256)
-                                while len(data) < 5:
-                                    data += conn.recv(256)
+                        if "PING" in data.decode("ascii"):
+                            conn.sendall("PONG\n".encode("ascii"))
+                    except socket.timeout:
+                        pass
+                    except:
+                        print("[+] Connection to relay server is closed.")
+                        break
 
-                                if "PING" in data.decode("ascii"):
-                                    conn.sendall("PONG\n".encode("ascii"))
-                            except socket.timeout:
-                                pass
-                            except:
-                                print("[+] Connection to relay server is closed.")
-                                break
+                    continue
 
-                            continue
+                cmd = self._cmdQueue.pop()
+                try:
+                    conn.sendall("{0}\n".format(cmd).encode("ascii"))
+                except:
+                    self._cmdQueue.insert(0, cmd)
+                    break
 
-                        cmd = self._cmdQueue.pop()
-                        try:
-                            conn.sendall("{0}\n".format(cmd).encode("ascii"))
-                        except:
-                            self._cmdQueue.insert(0, cmd)
-                            break
-
-                    conn.close()
+            conn.close()
 
     def sendCommand(self, cmd):
         self._cmdQueue.append(cmd)
